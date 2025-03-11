2b339da6347ce84e73929fabf35933eac84a6ce8
jihoon <tjwlgns2004@naver.com>
2025-03-11 16:13:53 +0900

    first

---
+++ src/test/java/com/example/springmybatisgradle/SpringMybatisGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmybatisgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybatisGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
